1b51010b5f881c9c44a5bf60264b42a52f2afbc8
uff20xd <donut121008@gmail.com>
2025-05-06 17:32:21 +0200

    k

--- shell.nix
+++ shell.nix
@@ -6,6 +6,6 @@ pkgs.mkShell {
     htmlhint
   ];
   shellHook = ''
-    nvim index.html my_opinions/music/index.html my_opinions/tv-things/index.html homepage.css my_opinions/music/musicpage.css 
+    nvim index.html my_opinions/music/index.html my_opinions/tv-things/index.html homepage.css my_opinions/music/musicpage.css my_opinions/tv-things/tvpage.css
   '';
 }
